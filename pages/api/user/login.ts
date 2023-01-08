1129c24d1209bdd66e8740a6a29da976c44b7c0c
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-01-08 21:42:49 +0700

    add portal

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -10,6 +10,13 @@ import { decryptRSA } from "../../../lib/rsa";
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "POST": {
+      res.setHeader('Access-Control-Allow-Credentials', "true")
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
       try {
         await loginSchema.validate(req.body);
         const username = req.body?.username
